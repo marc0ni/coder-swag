3b1ef39314d4fac1704c628f31d87606d3903536
Mark Lindamood <mark.lindamood@yahoo.com>
2017-09-15 22:07:12 -0700

    Completed IB for collection views

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Mark Lindamood on 9/15/17.
+//  Copyright © 2017 udemy. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
